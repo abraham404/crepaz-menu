adb5dfc0999f56cec18d34100f03456a7a7d7025
abraham <abraham.work7@gmail.com>
2026-04-03 14:26:21 -0700

    update menu

--- menus.js
+++ menus.js
@@ -179,8 +179,28 @@ const menus = {
     { nombre: "Vaso con leche", precio: "$55" },
     { nombre: "Agregar avena 35 grs al licuar por", precio: "$13" },
     { nombre: "(Malteada) Fresa, Vainilla o Chocolate", precio: "$100" },
+  ],
 
-    
+
+  hamburguesas_y_snacks: [
+    { nombre: "Hamburguesa Clásica carne 90 grs + $40 agrega papas fritas", precio: "$115" },
+    { nombre: "Guacamole Burger carne 90 grs (tocino 2 tiras) y guacamole con tomate, cebolla y cilantro) incluye papas fritas.", precio: "$195" },
+    { nombre: "Hamburguesa Americana carne 90 grs (huevo estrellado y champiñones) incluye papas fritas", precio: "$175" },
+    { nombre: "Hamburguesa Carnívora carne 90 grs (peperoni  y tocino 2 tiras ) incluye papas fritas", precio: "$195" },
+    { nombre: "Hamburguesa DOBLE  (2 carnes 90 grs c/u)(queso manchego y amarillo) incluye papas fritas", precio: "$195" },
+    { nombre: "Hamburguesa Hawaiana  carne  90 grs (piña, tocino  2 tiras, queso manchego y amarillo) incluye papas  fritas", precio: "$195" },
+    { nombre: "Todas las hamburguesas llevan tomate, aguacate, lechuga, cebolla, mayonesa, mostaza y cátsup.", precio: "" },
+    { nombre: "Torta de Jamón (2 rebanadas y queso amarillo)", precio: "$90" },
+    { nombre: "Torta de Pierna , Deshebrada o Pollo (carne 50 grs)", precio: "$115" },
+    { nombre: "Torta de carne asada.", precio: "$130" },
+    { nombre: "+ $40   agrega papas fritas", precio: "" },
+    { nombre: "*Las tortas llevan  tomate, aguacate,  lechuga,  cebolla, mayonesa, mostaza y crema.", precio: "" },
+    { nombre: "Orden de papas fritas (300 GRS)", precio: "$115" },
+    { nombre: "Nachos clásicos (Totopos con queso amarillo, salsa mexicana, frijol, crema y jalapeños)", precio: "$135" },
+    { nombre: "Nachos con carne asada (70 GRS) (Totopos con queso amarillo, salsa mexicana, frijol, crema, jalapeños)", precio: "$200" },
+    { nombre: "Salchipapas (papas 300 grs, 2 salchichas con tocino)", precio: "$135" },
+    { nombre: "Salchipapas con queso de nachos (papas 300 grs salchichas con tocino(2)", precio: "$165" },
+   
   ],
 
   bebidas: [
@@ -189,13 +209,32 @@ const menus = {
     { nombre: "Agua mineral (topo chico 600ml)", precio: "$45" },
     { nombre: "Botella agua 600ml", precio: "$25" },
     { nombre: "Botella agua 1 lt", precio: "$30" },
+    { nombre: "Aguas frescas 500ml", precio: "$40" },
     { nombre: "Limonada (natural o mineral) 473 ml", precio: "$75" },
     { nombre: "Naranjada (natural o mineral) 473 ml", precio: "$75" }
   ],
 
+combos: [
+  { nombre: "#1 Fruta con yoghurt, granola  y  miel, una bebida a elegir de  225 ml (jugo de naranja o café o te) ", precio: "$145" },
+  { nombre: "#2 Huevos revueltos con jamón, y café  o  te, jugo de naranja. (bebida de 225 ml ) tortillas y salsa servido con 2  guarniciones  a escoger (frijol, papas fritas o chilaquiles rojos o verdes)", precio: "$190" },
+  { nombre: "#3 Super desayuno: crepa pequeña con jarabe de maple, jamón a la plancha (2), huevos revueltos (3) y salsa. agrega taza de café o te  225 ml por $18.00 mas", precio: "$160" },
+  { nombre: "#4 Burger pack hamburguesa carne 90 grs , papas fritas y refresco 355 ml o agua fresca", precio: "$170" },
+  { nombre: "#5 Chilaquiles rojos o verdes con pollo (60 grs) o huevo (2) y taza de café regular  o  te 225 ml", precio: "$165" },
+  { nombre: "#6 Torta pack torta de pierna o deshebrada(50 grs), papas fritas y refresco 355 ml o agua fresca", precio: "$170" },
+  { nombre: "#7 Orden quesadillas (3) , café o jugo de naranja 225 ml. servido con frijol,  papas  fritas y salsa.", precio: "$165" },
+  { nombre: "#8 Crepa de nutella con fresas y frappe de mocha 473 ml", precio: "$200" },
+  { nombre: "#9 Club sandwich   (pan 3), jamón, queso amarillo, lechuga, tomate, aguacate, cebolla, pollo, tocino (50 grs)cebolla, tomate aguacate y lechuga, papas fritas 100 grs. y refresco de 355 ml.", precio: "$195" },
+  { nombre: "#10 Hot cakes (2) con huevos revueltos(2), salchichas(2) y café 225 ml ó té ml, mermelada, maple, mantequilla.", precio: "$175" },
+  { nombre: "#11 Desayuno americano hot cakes (3) con huevos revueltos(2), 2 tiras de tocino y una porcion pequeña de fruta. incluye jugo de naranja 225 ml. mermelada, maple, mantequilla. incluye salsa.", precio: "$195" },
+  { nombre: "#12 Hot dog pack un hot dog sencillo, papas fritas (100gr) y agua fresca o refresco de 355 ml.", precio: "$110" },
+  { nombre: "#13 Dos hot dogs sencillos y agua fresca o refresco de 355 ml.", precio: "$115" },
+  { nombre: "Los Combos no estan sujetos a modificaciones o sustitucion de ingredientes.", precio: "" },
+],
+
   cafe: [
     { nombre: "Regular Americano", precio: "$45 / $50 / $55" },
-    { nombre: "Espresso (SENCILLO/DOBLE) ", precio: "$50 / $55" },
+    { nombre: "Espresso (SENCILLO 28.41 ml /DOBLE 56.82 ml)", precio: "$40 / $50" },
+    { nombre: "Espresso Americano", precio: "$55 / $60 / $65" },
     { nombre: "Cappuccino  o Latte ", precio: "$70 / $80 / $90" },
     { nombre: "Cappuccino Caramelo o Vainlla", precio: "$80 / $90 / $100" },
     { nombre: "Cappuccino Nutella", precio: "$90 / $100 / $110" },
@@ -238,6 +277,16 @@ const menus = {
     { nombre: "Mocha", precio: "$110" },
     { nombre: "Cereza", precio: "$110" },
 
+  ],
+
+  hot_cakes: [
+    { nombre: "Hot cakes (3 pzas) Mermelada, maple, mantequilla", precio: "$130" },
+    { nombre: "Hot cakes (3 pzas) con una porción pequeña de fruta", precio: "$150" },
+    { nombre: "Hot Cakes (3) con Jamón o salchicha (2). Mermelada, maple, mantequilla.", precio: "$150" },
+    { nombre: "Sándwich de Jamón y Queso (pan blanco o integral)", precio: "$90" },
+    { nombre: "Sándwich de Pollo (50 grs) (pan blanco o integral) + $40 agrega papas fritas", precio: "$100" },
+    { nombre: "Fruta Fresca (variedad de fruta de temporada)", precio: "$105" },
+    { nombre: "Fruta con yogurt, Granola y miel de abeja", precio: "$115" },
   ],
 
     estimado_cliente: [
